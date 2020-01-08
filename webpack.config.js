24629abc05f2e2e7af43ab90c9d008ba9e3c1777
Singha360 <35334787+Singha360@users.noreply.github.com>
2020-01-08 04:30:52 -0500

    Provided polyfills for node modules

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,8 @@ module.exports = (env, argv) => {
       filename: "index.js"
     },
     node: {
-      __dirname: false,
-      __filename: false
+      __dirname: true,
+      __filename: true
     },
     module: {
       rules: [
